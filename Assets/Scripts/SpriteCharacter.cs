7d32e4e828ede3136ea102cfbf7b146348fed224
Adam Moen <C19318671@mytudublin.ie>
2022-12-09 17:14:53 +0000

    bla

---
+++ Assets/Scripts/SpriteCharacter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCharacter : MonoBehaviour
+{
+    private float hoz;
+    private float speed = 8f;
+    private float jump = 16f;
+    private bool isRight = true;
+
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hoz = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jump);
+        } 
+
+        if (Input.GetButtonUp("Jump") && body.velocity.y > 0f)
+        {
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(hoz * speed, body.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
